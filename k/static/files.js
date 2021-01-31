554a7e94728c59bed7cd5e88a0d9043b7fdc874c
David Ilchmann <david.ilchmann@gmail.com>
2021-01-31 13:32:48 +0100

    Delete k directory

--- k/static/files.js
+++
@@ -1,7 +0,0 @@
-const längeBtn = document.getElementById('länge-ändern-btn');
-const formFirstPart = document.getElementById('first-part');
-
-
-$("#länge-ändern-btn").on( "click", function() {
-    $('#first-part').toggle();
-})
